2b377e71aacf0616459af1345fb8003c0302215f
Madays <maday.choque@gmail.com>
2017-01-24 18:35:18 -0500

    prueba con escuchar evento click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //Escribir en el input de texto (id = mensaje)
 var mensajes = document.getElementById('mensajes');
-
+var chat = document.getElementById('chat');
 mensajes.addEventListener('click', onMensajesClick);
 
 function onMensajesClick(evt)
@@ -11,5 +11,31 @@ function onMensajesClick(evt)
 function agregarMensaje()
 {
     //si el ultimo teclado es enter, agregar el mesaje tipeado en id=chat
+    var longMensajes = mensajes.value.length;
+    if(mensajes!==""){
+        //si preciono enter o click 
+        if(mensajes.value[longMensajes-1] == "a"){
+            //creeun elementos para nombre, texto y hora
+            var abuela = document.createElement("div");
+            abuela.classList = "w-message w-message-in";
+            var madre = document.createElement("div");
+            madre.classList = "w-message-text";
+            var h5NombreHijo = document.createElement("h5");
+            
+            var pMensajesHijo = document.createElement("p");
+            pMensajesHijo.innerHTML=mensajes.value;
+            var divHoraHijo = document.createElement("div");
+            //cree un elementos para nombre texto hora***END
+            
+            //Creando paquete mensaje
+            madre.appendChild(h5NombreHijo);
+            madre.appendChild(pMensajesHijo);
+            madre.appendChild(divHoraHijo);
+            abuela.appendChild(madre); 
+            chat.appendChild(abuela);
+            mensajes.value="";
+        }    
+    }
+    
     
 }
