94017ab0a9c66b321aaf2615f12f9257b4a985b5
Madays <maday.choque@gmail.com>
2017-01-24 19:02:23 -0500

    limpiando master

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-//Escribir en el input de texto (id = mensaje)
-var mensajes = document.getElementById('mensajes');
 
-mensajes.addEventListener('click', onMensajesClick);
-
-function onMensajesClick(evt)
-{
-	agregarMensaje();
-}
-
-
-
-function agregarMensaje()
-{
-    //si el ultimo teclado es enter, agregar el mesaje tipeado en id=chat
-    
-}
