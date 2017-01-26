5af4ebaa564d4fec24279beb2ffd59a11d457704
Madays <maday.choque@gmail.com>
2017-01-26 13:02:52 -0500

    intentando actualizar header

--- js/main.js
+++ js/main.js
@@ -1,27 +1,34 @@
 /*------------Agregar Mensaje-------------*/
-/*var mensajes = document.getElementById('mensajes');
+var mensajes = document.getElementById('mensajes');
 var chat = document.getElementById('chat');
-mensajes.addEventListener('click', onMensajesClick);
+mensajes.addEventListener('keyup', onMensajesClick);
 
-function onMensajesClick(evt){agregarMensaje();}
+function onMensajesClick(evt){
+    //si preciono enter     
+    if(evt.keyCode==13){
+        agregarMensaje();    
+    }
+    }
 
 function agregarMensaje()
 {
-    //si el ultimo teclado es enter, agregar el mesaje tipeado en id=chat
-    var longMensajes = mensajes.value.length;
-    if(mensajes!==""){
-        //si preciono enter o click 
-        if(mensajes.value[longMensajes-1] == "a"){
+    //Agregar hora
+            var fecha = new Date();
+            var hora = fecha.getHours()+":"+fecha.getMinutes();
+            window.status=hora;
+    //agregar el mesaje tipeado en id=chat
+    if(mensajes.value!==""){       
             //creeun elementos para nombre, texto y hora
             var abuela = document.createElement("div");
-            abuela.classList = "w-message w-message-in";
+            abuela.classList = "w-message w-message-out";
             var madre = document.createElement("div");
             madre.classList = "w-message-text";
             var h5NombreHijo = document.createElement("h5");
-            
             var pMensajesHijo = document.createElement("p");
             pMensajesHijo.innerHTML=mensajes.value;
             var divHoraHijo = document.createElement("div");
+            divHoraHijo.innerHTML=hora;
+            divHoraHijo.style.float="right";
             //cree un elementos para nombre texto hora***END
             
             //Creando paquete mensaje
@@ -29,27 +36,32 @@ function agregarMensaje()
             madre.appendChild(pMensajesHijo);
             madre.appendChild(divHoraHijo);
             abuela.appendChild(madre); 
-            chat.appendChild(abuela);
-            mensajes.value="";
-        }    
+            chat.appendChild(abuela);        
+            mensajes.value="";      
+            //Scroll 
+            chat.scrollTop = chat.scrollHeight;            
     }   
-}*/
+}
 /*---------END---Agregar Mensaje-------------*/
 
 /*------------Seleccionar contacto-------------*/
 //Al seleccionar los li de ul class="w-recent-chats debo obtener los datos img, nombre y texto y los datos img y nombre ponerlo en header tmb actualizar el campo chat"
-
-    //obteniendo datos img, nombre    
-    var chat = document.getElementById('chat');
-    var contacto = document.getElementsByClassName('avatar');
-    contacto[0].addEventListener('click', onContactoClick);
-    
-
-    //Actualizando ventana chat
+    var contactos = document.getElementsByClassName('w-recent-chats');
+    contactos[0].addEventListener('click', onContactoClick);
     function onContactoClick(evt){
-        if(evt.target){
-            chat.innerHTML="" ;
-        }
+        //obteniendo datos img, nombre    
+        var datosAvatar = evt.target.parentElement.firstChild.nextElementSibling;
+        console.log(datosAvatar);
+        var  datoImg=datosAvatar;
+        var cImg = document.getElementsByClassName('wh-44');
+        var cNombre = document.getElementsByClassName('w-contact-name');
+        //Actualizando ventana chat
+        var chat = document.getElementById('chat');
+        chat.innerHTML="";
+        //Actualizando header con nuevo contacto
+        var wChatProfile = document.getElementsByClassName('w-chat-profile');
+        wChatProfile[0].appendChild(datoImg);
+        //wChatProfile[0].append(datosAvatar);
     }
     
 /*---------END---Seleccionar contacto-------------*/
