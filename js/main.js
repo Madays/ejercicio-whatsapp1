d89fa095efa1030446845171a8edcbe651782544
Madays <maday.choque@gmail.com>
2017-01-27 12:29:14 -0500

    probando header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+//Actualizar header
+var imgContact = document.getElementById("imgContact");
+var contactName = document.getElementById("contactName");
+var listContact = document.getElementById("listContact");
+var chat = document.getElementById("chat");
+var avatarChat = document.getElementsByClassName("avatarChat");
+var arreglo = [];
+var arregloContact = [];
 
+arreglo.push(avatarChat);
+arregloContact.push(listContact);
+avatarChat[0].addEventListener("click",function(){
+    imgContact.src = this.children[0].src;
+    contactName.textContent = this.children[1].textContent; 
+    listContact.innerHTML= '<li>'+'Ana María, '+'</li>'+
+	  							'<li>'+'Aldo, '+'</li>'+
+	  							'<li>'+'Gian, '+'</li>'+
+	  							'<li>'+'Mariana, '+'</li>'+
+	  							'<li>'+'Papu, '+'</li>'+
+	  							'<li>'+'Tú'+'</li>';
+                               });
+for(var i=1; i<=avatarChat.length; i++){    
+    avatarChat[i].addEventListener("click",changeHeader);
+}
+
+function changeHeader(evt){
+    
+    chat.innerHTML="";
+    imgContact.src = this.children[0].src;
+    contactName.textContent = this.children[1].textContent;
+    //listContact.children = arregloContact.removeChild[0]; 
+    listContact.innerHTML="";
+}
+//----------END---Actualizar header
