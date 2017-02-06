1c4b132f48ca280a874e9b01151a07770f767680
Madays <maday.choque@gmail.com>
2017-02-06 10:55:56 -0500

    objeto contact creado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+var recentChats = [];
+
+function Contact(_name,_avatar,_message){
+    this.name=_name;
+    this.avatar=_avatar;
+    this.message=_message;
+}
+var newContact = new Contact('Mad',':)','Hi');
+console.log(newContact);
+function pushChats(){
+    recentChats.push([newContact.name,newContact.avatar,newContact.message]);
+}
 
