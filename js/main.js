c6adc6667914ee76eb8e1ae3c5589c2b3f975063
Madays <maday.choque@gmail.com>
2017-02-06 11:10:45 -0500

    pushChats

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var recentChats = [];
 
-function Contact(_name,_avatar,_message){
+function Contact(_name,_avatar){
     this.name=_name;
     this.avatar=_avatar;
-    this.message=_message;
+    this.message=document.getElementById('mensajes').value;
 }
-var newContact = new Contact('Mad',':)','Hi');
+var newContact = new Contact('Mad',':)');
 console.log(newContact);
 function pushChats(){
     recentChats.push([newContact.name,newContact.avatar,newContact.message]);
