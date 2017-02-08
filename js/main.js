12e8b7ef1b466d926be706635b2f94f240bcefa6
Madays <maday.choque@gmail.com>
2017-02-08 08:45:44 -0500

    buscador

--- js/main.js
+++ js/main.js
@@ -16,6 +16,25 @@ var arrAlmacenarChat = [];
 function pushMensajes(_mensaje){
     arrAlmacenarChat.push({mensaje:_mensaje,hora:hora.getHours(),minutos:hora.getMinutes()});
 }
+
+/*-------------buscador------*/
+
+var search = document.getElementById("search");
+var contacto = document.getElementsByTagName("h4");
+var forEach = Array.prototype.forEach;
+
+search.addEventListener("keyup", function(e){
+  var choice = this.value;
+
+  forEach.call(contacto, function(f){
+      if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
+          f.parentNode.parentNode.style.display = "none";   
+      else
+         f.parentNode.parentNode.style.display = "block";        
+  });
+}, 
+false);
+/*-------------end buscador------*/
 //Parte visual
 var liListItem = null;
 function init(){    
